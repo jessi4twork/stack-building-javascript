f2c7a149925a0b98566808d14d739fc7ad1e1e83
Jessi Martinez <jessi4twork@gmail.com>
2026-04-03 22:43:33 +0800

    Create script.js

---
+++ trivia-robot/script.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Dug";
+let botLocation = "the computer";
+let favoriteLanguage = "JavaScript";
+
+const botIntroduction = ("My name is " + botName + " and I live on " + botLocation + ".");
+console.log(botIntroduction);
+
+const botLanguage = ("My favorite programming language is " + favoriteLanguage + ".");
+console.log(botLanguage);
+
+let codingFact = ("Fun fact! Did you know that " + favoriteLanguage + " was invented out as a party trick?");
+console.log(codingFact);
+
+codingFact = ("Okay, let me correct myself. " + favoriteLanguage + "was someone's imaginary language when they were a child.")
+console.log(codingFact);
+
+codingFact = ("Fine. You got me. This is really the fact." + favoriteLanguage + "was invented out of a need to make the early web more interactive.")
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
