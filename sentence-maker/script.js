86b6590a214408ffd4e35fe66cb93fa7fc6a14bb
Jessi Martinez <jessi4twork@gmail.com>
2026-04-06 21:14:22 +0800

    Create script.js

---
+++ sentence-maker/script.js
@@ -0,0 +1,20 @@
+let adjective = "sad";
+let noun = "boy";
+let verb = "angry";
+let place = "remote area";
+let adjective2 = "pointy";
+let noun2 = "plants";
+
+const firstStory = ("Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".");
+console.log("First story: " + firstStory);
+
+adjective = "yummy";
+noun = "dog";
+verb = "hungry";
+place = "shell";
+adjective2 = "black";
+noun2 = "everything";
+
+const secondStory = ("Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".");
+console.log("Second story: " + secondStory);
+
