4c7625a7207387500967d53ad7ac14e745e0f3d5
Jessi Martinez <jessi4twork@gmail.com>
2026-05-07 08:32:12 +0800

    Create script.js

---
+++ card-counting-assistant/script.js
@@ -0,0 +1,24 @@
+let count = 0;
+function cardCounter(card) {
+  switch (card) {
+    case 2: 
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+  
+  let holdBet = count > 0 ? "Bet" : "Hold";
+
+  return count + " " + holdBet;
+}
+
