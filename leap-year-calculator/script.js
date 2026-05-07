c5b70fab30f5f7bc0a50f757967b2f182421cc36
Jessi Martinez <jessi4twork@gmail.com>
2026-05-07 08:44:03 +0800

    Create script.js

---
+++ leap-year-calculator/script.js
@@ -0,0 +1,14 @@
+function isLeapYear(year) {
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return year + " is a leap year.";
+  }
+  else {
+    return year + " is not a leap year.";
+  }
+}
+
+let year = 2024;
+let result = isLeapYear(year);
+
+console.log(result);
+
