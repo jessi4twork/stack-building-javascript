46c9df5e734e31dc57cfa0abc85b78c2c87567f6
Jessi Martinez <jessi4twork@gmail.com>
2026-06-01 17:00:51 +0800

    Create script.js

---
+++ recipe-tracker/script.js
@@ -0,0 +1,34 @@
+const recipes = [];
+
+const recipe1 = {
+  name: "Spaghetti Carbonara",
+  ingredients: ["spaghetti", "Parmesan cheese", "pancetta", "black pepper"],
+  cookingTime: 22,
+  totalIngredients: null,
+  difficultyLevel: ""
+};
+
+const recipe2 = {
+  name: "Chicken Curry",
+  ingredients: ["chicken breast", "coconut milk", "curry powder", "onion", "garlic"],
+  cookingTime: 42,
+  totalIngredients: null,
+  difficultyLevel: ""
+};
+
+const recipe3 = {
+  name: "Vegetable Stir Fry",
+  ingredients: ["broccoli", "carrot", "bell pepper"],
+  cookingTime: 15,
+  totalIngredients: null,
+  difficultyLevel: ""
+};
+
+recipes.push(recipe1, recipe2, recipe3);
+
+const getTotalIngredients = (ingredientsArray) => {
+  const countIngredients = ingredientsArray.length;
+  return countIngredients;
+};
+
+console.log(getTotalIngredients(recipe1.ingredients));
