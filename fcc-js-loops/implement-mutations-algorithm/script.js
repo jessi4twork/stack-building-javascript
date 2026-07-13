92bc327d24b5c9e42b0c4cbb76bc19f8eced7543
jessi4twork <jessi4twork@gmail.com>
2026-07-13 14:04:05 +0800

    Add lab for implementing mutations algorithm

---
+++ fcc-js-loops/implement-mutations-algorithm/script.js
@@ -0,0 +1,24 @@
+function mutation(arr) {
+  const first = arr[0].toLowerCase();
+  const second = arr[1].toLowerCase();
+
+  for(let i = 0; i < second.length; i++){
+    if(first.indexOf(second[i]) === -1){
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(mutation(["hello", "hey"]));
+console.log(mutation(["hello", "Hello"]));
+console.log(mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]));
+console.log(mutation(["Mary", "Army"]));
+console.log(mutation(["Mary", "Aarmy"]));
+console.log(mutation(["Alien", "line"]));
+console.log(mutation(["floor", "for"]));
+console.log(mutation(["hello", "neo"]));
+console.log(mutation(["voodoo", "no"]));
+console.log(mutation(["ate", "date"]));
+console.log(mutation(["Tiger", "Zebra"]));
+console.log(mutation(["Noel", "Ole"]));
