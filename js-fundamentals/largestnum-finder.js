cdd9df868d35e97ac08dd49b390a4358ab35fed3
jessi4twork <jessi4twork@gmail.com>
2026-08-21 11:44:37 +0800

    Added freeCodeCamp Largest Number lab.

---
+++ js-fundamentals/largestnum-finder.js
@@ -0,0 +1,25 @@
+function largestOfAll(arr) {
+  let result = [];
+  for(let i = 0; i < arr.length; i++) {
+    const maxArr = Math.max(...arr[i]);
+    result.push(maxArr)
+  }
+  return result;
+}
+
+console.log(largestOfAll([[4, 5, 1, 3], 
+  [13, 27, 18, 26], 
+  [32, 35, 37, 39], 
+  [1000, 1001, 857, 1]]));
+console.log(largestOfAll([[13, 27, 18, 36], 
+  [4, 5, 1, 3], 
+  [32, 35, 37, 39], 
+  [1000, 1001, 857, 1]]));
+console.log(largestOfAll([[4, 9, 1, 3], 
+  [13, 35, 18, 26], 
+  [32, 35, 97, 39], 
+  [1000000, 1001, 857, 1]]));
+console.log(largestOfAll([[17, 23, 25, 12], 
+  [25, 7, 34, 48], 
+  [4, -10, 18, 21], 
+  [-72, -3, -17, -10]]));
