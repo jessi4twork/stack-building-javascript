bc76b0b66c1ad43906b01faf25d3900534728186
jessi4twork <jessi4twork@gmail.com>
2026-08-24 16:27:41 +0800

    Added freeCodeCamp Slice and Splice Algorithm.

---
+++ js-fundamentals/slicesplice-algorithm.js
@@ -0,0 +1,25 @@
+function frankenSplice (arr1, arr2, index) {
+  let result = [...arr2];
+  result.splice(index, 0, ...arr1);
+  return result;
+}
+
+console.log(frankenSplice([1, 2, 3],
+  [4, 5],
+  1
+));
+
+console.log(frankenSplice([1, 2],
+  ["a", "b"], 1
+));
+
+console.log(frankenSplice(["claw", "tentacle"],
+  ["head", "shoulders", "knees", "toes"],
+  2
+));
+
+console.log(frankenSplice(
+  [1, 2, 3, 4],
+  [],
+  0
+));
